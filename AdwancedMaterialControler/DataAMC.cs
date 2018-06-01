874e86531593d497bd9d55eac3313876255baa2a
Marcin Zboralski <34725819+ReMakeX@users.noreply.github.com>
2018-06-01 06:42:08 +0200

    v0.2a Not Hermeted

---
+++ AdwancedMaterialControler/DataAMC.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace AdwancedMaterialControler
+{
+    [System.Serializable]
+    public sealed class DataAMC
+    {
+        public string DataName;
+        public Shader Shader;
+        public Texture[] Textures;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AMC_Render
+
